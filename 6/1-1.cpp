e3dd21da105953c4a6bd97dba8c6719e07bfdd20
홍석현 <114850357+fr35wo@users.noreply.github.com>
2022-10-10 21:19:19 +0900

    Rename 1-1.cpp to 6/1-1.cpp

--- 6/1-1.cpp
+++ 6/1-1.cpp
@@ -4,7 +4,7 @@
 #include <string>
 using namespace std;
 //1-1
-//Test a[2]; ������ ����� �� � �޼����� ��µɱ�?
+//Test a[2]; 문장이 실행될 때 어떤 메세지가 출력될까?
 
 class Test {
 	int x;
@@ -16,5 +16,5 @@ public:
 int main() {
 	Test a[2];
 }
-//0 0�� ��µȴ�. ��ü �迭 Test�� ũ�⸦ 2�� �ʱ�ȭ �� �� ���� 
-//�������� �ʾ� ����Ʈ �μ��� 0�� ��µǴ� ���̴�.
+//0 0이 출력된다. 객체 배열 Test의 크기를 2로 초기화 한 후 값을 
+//설정하지 않아 디폴트 인수인 0이 출력되는 것이다.
