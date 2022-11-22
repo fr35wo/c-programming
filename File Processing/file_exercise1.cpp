1f6b5f74a9c068ca1e966a893799e51f618cd080
홍석현 <114850357+fr35wo@users.noreply.github.com>
2022-11-22 16:10:59 +0900

    sample1

---
+++ File Processing/file_exercise1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	ofstream os{ "sample1.txt" };
+	if (!os) {
+		cerr << "파일 오픈에 실패하였습니다" << endl;
+		exit(1);
+	}
+	for (int i = 0; i < 10; i++)
+		os << i << " ";
+	return 0;
+	// 객체 os가 범위를 벗어나면 ofstream 소멸자가 파일을 닫는다.
+}
